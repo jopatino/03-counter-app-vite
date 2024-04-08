2c9c8704260ab15c6faa527137200de17142f889
jopatino <jopatino86@misena.edu.co>
2024-04-08 17:38:34 -0500

    return elements in the component

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,3 +1,30 @@
+// export const FirstApp = () => {
+//     return (
+//         <div class="row">
+//             <h1>Jose Patiño</h1>
+//             <p>im a subtitle</p>
+//         </div>
+//     )
+// }
+
+// Nueva forma para no agregar el div//////////////////
+// import { Fragment } from "react";
+//
+// export const FirstApp = () => {
+//     return (
+//         <Fragment>
+//             <h1>Jose Patiño</h1>
+//             <p>im a subtitle</p>
+//         </Fragment>
+//     )
+// }
+
+// Sinónimo del fragmento////////////////
 export const FirstApp = () => {
-    return <h1>Jose Patiño</h1>
+    return (
+        <>
+            <h1>Jose Patiño</h1>
+            <p>im a subtitle</p>
+        </>
+    )
 }
